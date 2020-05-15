7dff765d39c82cff89c0bf7f80ec6db82b93c0fd
ankitdubey1902 <61725942+ankitdubey1902@users.noreply.github.com>
2020-05-15 10:58:45 +0530

    Master
    First Commit

--- src/data/data.js
+++ src/data/data.js
@@ -4,27 +4,34 @@ export const jobsAvailable = [
     company: "Google",
     title: "Senior Software Engineer",
     location: "Mountain View, CA",
-    datePosted: "8 days ago"
+    datePosted: "8 days ago",
   },
   {
     id: 1,
     company: "Facebook",
     title: "Junior UI/UX Designer",
     location: "New York, NY",
-    datePosted: "5 days ago"
+    datePosted: "5 days ago",
   },
   {
     id: 2,
     company: "Medium",
     title: "Cloud Architect Engineer",
     location: "San Francisco, CA",
-    datePosted: "3 days ago"
+    datePosted: "3 days ago",
   },
   {
     id: 3,
     company: "Netflix",
     title: "Data Scientist",
     location: "Los Angeles, CA",
-    datePosted: "2 days ago"
-  }
+    datePosted: "2 days ago",
+  },
+  {
+    id: 4,
+    company: "Microsoft",
+    title: "Data Scientist",
+    location: "Mumbai, India",
+    datePosted: "1 days ago",
+  },
 ];
