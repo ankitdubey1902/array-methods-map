543395cc11d6715458020e4af38e80cfb1b36316
ankitdubey1902 <61725942+ankitdubey1902@users.noreply.github.com>
2020-05-17 12:18:20 +0530

    Updates

---
+++ precache-manifest.5970bea853568092b37b671b1882b104.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e895618d2a6a68ddf7e5c4896ebc7fbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db09b3ad3bdc0e97d10a",
+    "url": "/static/css/main.343ff9b7.chunk.css"
+  },
+  {
+    "revision": "39829c0a2e5580b8771c",
+    "url": "/static/js/2.ae9a8980.chunk.js"
+  },
+  {
+    "revision": "db09b3ad3bdc0e97d10a",
+    "url": "/static/js/main.2e858297.chunk.js"
+  },
+  {
+    "revision": "e6246cc6de195eddb928",
+    "url": "/static/js/runtime~main.9bb38e2d.js"
+  }
+]);
